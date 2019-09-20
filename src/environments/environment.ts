9a5f6c1ac1bffb74cb1887b9267686224e045d9d
Reynir Aron <reyniraron@gmail.com>
2019-09-20 09:24:50 +0000

    Add AngularFire and Firestore integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB3GVRhB7U2M5cp7IK1uGqprMFwaOEDqoU',
+    authDomain: 'amigo-c73c8.firebaseapp.com',
+    databaseURL: 'https://amigo-c73c8.firebaseio.com',
+    projectId: 'amigo-c73c8',
+    storageBucket: '',
+    messagingSenderId: '230512385233',
+    appId: '1:230512385233:web:bc99d64c0594b2f7bbf9f0'
+  }
 };
 
 /*
