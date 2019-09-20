11269fcf33c433a2a0063ee7d01c3e0b83cb549a
Reynir Aron <reyniraron@gmail.com>
2019-09-20 09:56:58 +0000

    Add Firebase authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amigo';
+  constructor(public afAuth: AngularFireAuth) {}
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
