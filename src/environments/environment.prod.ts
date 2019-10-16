34d3360eed0fc640079cfbacdecae2fc00c7dcda
Reynir Aron <reyniraron@gmail.com>
2019-10-16 22:07:43 +0000

    Set up build + deploy tasks for production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB3GVRhB7U2M5cp7IK1uGqprMFwaOEDqoU',
+    authDomain: 'amigo-c73c8.firebaseapp.com',
+    databaseURL: 'https://amigo-c73c8.firebaseio.com',
+    projectId: 'amigo-c73c8',
+    storageBucket: '',
+    messagingSenderId: '230512385233',
+    appId: '1:230512385233:web:bc99d64c0594b2f7bbf9f0'
+  }
 };
