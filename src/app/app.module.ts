e4c13deae7bb353789a706c817cc5714e0fb81eb
Victor-733 <victorwahid@gmail.com>
2019-10-23 15:04:32 +0000

    do stuff, the sequel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
@@ -28,6 +29,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MatButtonModule,
     MatToolbarModule,
     MatMenuModule,
+    MatSidenavModule,
     MatIconModule,
     MatProgressBarModule,
   ],
