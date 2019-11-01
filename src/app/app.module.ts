50eb4c41c653f39eead1b7bd828de296c70b4942
Victor-733 <victorwahid@gmail.com>
2019-11-01 09:15:36 +0000

    add sidenav items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { HomeComponent } from './home/home.component';
@@ -31,8 +33,10 @@ import { HomeComponent } from './home/home.component';
     MatButtonModule,
     MatToolbarModule,
     MatMenuModule,
+    MatDividerModule,
     MatSidenavModule,
     MatIconModule,
+    MatListModule,
     MatProgressBarModule,
   ],
   providers: [],
