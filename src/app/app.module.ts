1f4f21620d6629829005d2b1d1e6e452a7c134cf
Victor-733 <victorwahid@gmail.com>
2019-11-13 14:21:46 +0000

    initialize settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,11 +19,13 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
