0821d1813e1cb1a8b539bc2b0b848181a71f3738
Reynir Aron <reyniraron@gmail.com>
2019-11-20 14:07:22 +0000

    Get profile information from database

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../database.service';
+import { Profile } from '../profile';
+import { AuthService } from '../auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  currentProfile: Profile;
 
-  constructor() { }
+  constructor(public db: DatabaseService, public auth: AuthService) {
+    // Create a default profile to use if for some reason fetching the information fails
+    this.currentProfile = {
+      uid: '',
+      name: '',
+      age: NaN,
+      location: '',
+      description: '',
+      imageUrl: ''
+    };
+  }
 
   ngOnInit() {
+    // Go through profiles and find user's own profile
+    this.db.profiles.forEach(profiles => profiles.forEach(profile => {
+      if (!this.auth.uid) {
+        return;
+      }
+      if (profile.uid === this.auth.uid) {
+        this.currentProfile = profile;
+        return;
+      }
+    }));
   }
 
   updateSettings(event: Event) {
