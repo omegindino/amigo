4db7dbab72d191f6a096e1845edc50b5af9204ba
Victor-733 <victorwahid@gmail.com>
2019-11-29 09:50:26 +0000

    added a counter for number of friends

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,19 @@ import {
 })
 export class AppComponent {
   title = 'amigo';
+  friendsCount: number;
+  // to do: make this list so that the users here automatically get added to the page
+  onlineFriendsList = [
+    {name: "Victor_733", imageUrl: "https://lh3.googleusercontent.com/-wG7j5SzpXXY/VBwE7yvofEI/AAAAAAAAAAw/mf40YBhUugsqniIWBeglnDwX0PKnBQmZgCEwYBhgL/w140-h140-p/2014-09-19.jpg"},
+    {name: "Birkir", imageUrl: "https://cdn.discordapp.com/attachments/575446133103394827/649891994721583115/IMG-4612.JPG"},
+  ]
+  offlineFriendsList = [
+    {name: "Reynir Aron", imageUrl: "https://miro.medium.com/max/3150/1*VLD9nu6jQCdYRpj4EnXffw.png"}
+  ]
   loading = false;
 
   constructor(public authService: AuthService, private router: Router) {
+    this.friendsCount = this.onlineFriendsList.length + this.offlineFriendsList.length;
     // Loading progress bar
     this.router.events.subscribe((event: Event) => {
       switch (true) {
